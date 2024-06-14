f8680ba050e5291d20ca54fb1b25b059e35ab5f8
baniasbaabe <banias@hotmail.de>
2024-06-14 17:51:18 +0200

    add better messages and spinner

--- src/app.py
+++ src/app.py
@@ -18,31 +18,31 @@ st.set_page_config(page_title="Arbeitszeugnis.ai", page_icon="https://1000logos.
 
 st.title("🧠 Arbeitszeugnis.ai")
 
-file_path = st.file_uploader("Upload your job reference")
+file_path = st.file_uploader("Upload your Job Reference")
 
 if file_path:
-    
-    doc = fitz.open(stream = file_path.read(), filetype="pdf")
-    
-    for i, page in enumerate(doc):
-        pix = page.get_pixmap()
-        pix.save(f"page_{i}.png")
-        images.append(f"page_{i}.png")
-    
-    for image in images:
-        try:
-            job_reference_content += generate_response(ModelName.GEMINI_VISION, Content(TEXT_EXTRACTION_PROMPT, image))
-        except APIConnectionError:
-            st.error("API Connection Error: Please check if your country is eligible for the API https://ai.google.dev/available_regions.")
-            st.stop()
+    with st.spinner("Your Job Reference will be decoded. Hold on..."):
+        doc = fitz.open(stream = file_path.read(), filetype="pdf")
+        
+        for i, page in enumerate(doc):
+            pix = page.get_pixmap()
+            pix.save(f"page_{i}.png")
+            images.append(f"page_{i}.png")
+        
+        for image in images:
+            try:
+                job_reference_content += generate_response(ModelName.GEMINI_VISION, Content(TEXT_EXTRACTION_PROMPT, image))
+            except APIConnectionError:
+                st.error("API Connection Error: Please check if your country is eligible for the API https://ai.google.dev/available_regions. Or Gemini Pro API isn't free anymore :)")
+                st.stop()
 
-    JOB_REFERENCE_DECODER_PROMPT = JOB_REFERENCE_DECODER_PROMPT + job_reference_content
-    results = generate_response(ModelName.GEMINI_PRO, Content(JOB_REFERENCE_DECODER_PROMPT))
-    results = clean_results(results)
-    
-    reference_sents = find_matches(results, job_reference_content)
-    
-    dep_svg = render_displacy(job_reference_content, reference_sents)
-    st.markdown(dep_svg, unsafe_allow_html=True)
-    
-    remove_images(images)
+        JOB_REFERENCE_DECODER_PROMPT = JOB_REFERENCE_DECODER_PROMPT + job_reference_content
+        results = generate_response(ModelName.GEMINI_PRO, Content(JOB_REFERENCE_DECODER_PROMPT))
+        results = clean_results(results)
+        
+        reference_sents = find_matches(results, job_reference_content)
+        
+        dep_svg = render_displacy(job_reference_content, reference_sents)
+        st.markdown(dep_svg, unsafe_allow_html=True)
+        
+        remove_images(images)
